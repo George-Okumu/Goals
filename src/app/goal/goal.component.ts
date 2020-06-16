30ead56be28185a12d4af9f328ebb21a1eece85c
George Okumu <gokumu12@gmail.com>
2020-06-16 17:19:15 +0300

    created service class(goals.ts) and imported our array,created our goal service to e as dependacy of our goals array,created alertService to alert the user that their message have been deleted

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
 
 @Component({
   selector: 'app-goal',
@@ -7,35 +9,35 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-  goals:Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,7,1)),
-    new Goal(2, 'Buy cookies', 'I have to buy cookies for the parrot',new Date(2020,6,3)),
-    new Goal(3, 'Get new phone case', 'Diana has a birthday coming up soon',new Date(2020,6,7)),
-    new Goal(4, 'Get Dog food', 'Pupper likes expensive snacks',new Date(2020,5,9)),
-    new Goal(5, 'Solve math homework', 'Damn maths',new Date(2020,5,20)),
-    new Goal(6, 'Plot my world domination', 'Cause i am an evil over lord',new Date(2020,7,31))
-  ];
+  goals: Goal[];
+  alertService: AlertService;
+
+  constructor(goalService: GoalService, alertService: AlertService){
+    this.goals = goalService.getGoals();
+    this.alertService = alertService;
+  }
+
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
   deleteGoal(isCompleted, index){
-    if(isCompleted) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+    if (isCompleted) {
+      const toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`);
 
-      if(toDelete){
-        this.goals.splice(index,1)
+      if (toDelete){
+        this.goals.splice(index, 1);
+        this.alertService.alertMe('The goal has been deleted');
       }
     }
 
   }
   addNewGoal(goal){
-    let goalLength = this.goals.length;
-    goal.id= goalLength+1;
+    const goalLength = this.goals.length;
+    goal.id = goalLength+1;
     goal.completeDate = new Date(goal.completeDate);
-    this.goals.push(goal)
+    this.goals.push(goal);
   }
 
-  constructor() { }
 
   ngOnInit(): void {
   }
